aa7e32eee318cf5181d417c554e6123040919944
Harshad Sharma <harshad@harshadsharma.com>
2012-11-25 17:54:43 +0530

    Updating for user resource in Api

--- zuzee/toilet/api.py
+++ zuzee/toilet/api.py
@@ -1,7 +1,18 @@
+from django.contrib.auth.models import User
+from tastypie import fields
 from tastypie.resources import ModelResource
 from toilet.models import Toilet
 
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+
 class ToiletResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+    
     class Meta:
         queryset = Toilet.objects.all()
         resource_name = 'toilet'
+
--- zuzee/zuzee/urls.py
+++ zuzee/zuzee/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.views.decorators.csrf import csrf_exempt
-from toilet.views import AddView
-from toilet.api import ToiletResource
+#from toilet.views import AddView
+from tastypie.api import Api
+from toilet.api import UserResource, ToiletResource
 
 from django.contrib import admin
 admin.autodiscover()
 
-toilet_resource = ToiletResource()
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(ToiletResource())
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^toilet/add/$', csrf_exempt(AddView.as_view()), name='add'),
-    url(r'^api/', include(toilet_resource.urls)),
+    #url(r'^toilet/add/$', csrf_exempt(AddView.as_view()), name='add'),
+    url(r'^api/', include(v1_api.urls)),
 )
